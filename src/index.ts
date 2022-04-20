e01330fc351291dcf052a07460cf2c242af4036a
rowlandwilliams <rowland.williams.94@gmail.com>
2022-04-20 17:03:57 +0100

    add serverless

--- src/index.ts
+++ src/index.ts
@@ -3,44 +3,38 @@ import { Contribution } from "./entity/Contribution";
 import { convertRawContributionDataToDays } from "./helpers/helpers";
 import { AppDataSource } from "./data-source";
 
-AppDataSource.initialize()
-  .then(async () => {
-    const today = new Date();
-    const year = today.getFullYear();
+export const handler = () => {
+  AppDataSource.initialize()
+    .then(async () => {
+      const today = new Date();
+      const year = today.getFullYear();
 
-    const response = await fetch(
-      `https://skyline.github.com/rowlandwilliams/${year}.json`
-    );
+      const response = await fetch(
+        `https://skyline.github.com/rowlandwilliams/${year}.json`
+      );
 
-    // get raw contribution data
-    const contributionsData = await response.json();
+      // get raw contribution data
+      const contributionsData = await response.json();
 
-    // convert raw data into days of the year
-    const dailyContributions = convertRawContributionDataToDays(
-      contributionsData,
-      year
-    );
+      // convert raw data into days of the year
+      const dailyContributions = convertRawContributionDataToDays(
+        contributionsData,
+        year
+      );
 
-    // get latest contribution
-    const todaysContribution = dailyContributions.slice(-1)[0];
-    const { date, value } = todaysContribution;
+      // get latest contribution
+      const todaysContribution = dailyContributions.slice(-1)[0];
+      const { date, value } = todaysContribution;
 
-    // add to contributions table
-    const contribution = new Contribution();
-    contribution.date = date;
-    contribution.value = value;
+      // add to contributions table
+      const contribution = new Contribution();
+      contribution.date = date;
+      contribution.value = value;
 
-    await AppDataSource.manager.save(contribution);
+      console.log(contribution);
+      await AppDataSource.manager.save(contribution);
 
-
-    const loads = dailyContributions.slice(-10)
-    await AppDataSource
-    .createQueryBuilder()
-    .insert()
-    .into(Contribution)
-    .values(loads)
-    .execute()
-
-    console.log(`Saved contribution for ${date}`);
-  })
-  .catch((error) => console.log(error));
+      console.log(`Saved contribution for ${date}`);
+    })
+    .catch((error) => console.log(error));
+};
