eadf21a39348b5e518cc617205afed54ebadee6b
rowlandwilliams <rowland.williams.94@gmail.com>
2022-04-20 16:48:24 +0100

    add multiple years to seed

--- src/migration/seed-contributions.ts
+++ src/migration/seed-contributions.ts
@@ -3,29 +3,31 @@ import { Contribution } from "./../entity/Contribution";
 import { AppDataSource } from "./../data-source";
 import fetch from "node-fetch";
 
+const years = [2020, 2021, 2022];
+
 AppDataSource.initialize()
   .then(async () => {
-    const year = new Date().getFullYear();
+    years.forEach(async (year) => {
+      const response = await fetch(
+        `https://skyline.github.com/rowlandwilliams/${year}.json`
+      );
 
-    const response = await fetch(
-      `https://skyline.github.com/rowlandwilliams/${year}.json`
-    );
+      // get raw contribution data
+      const contributionsData = await response.json();
 
-    // get raw contribution data
-    const contributionsData = await response.json();
+      // convert raw data into days of the year
+      const dailyContributions = convertRawContributionDataToDays(
+        contributionsData,
+        year
+      );
 
-    // convert raw data into days of the year
-    const dailyContributions = convertRawContributionDataToDays(
-      contributionsData,
-      year
-    );
+      await AppDataSource.createQueryBuilder()
+        .insert()
+        .into(Contribution)
+        .values(dailyContributions)
+        .execute();
 
-    await AppDataSource.createQueryBuilder()
-      .insert()
-      .into(Contribution)
-      .values(dailyContributions)
-      .execute();
-
-    console.log(`Saved all contributions for ${year}`);
+      console.log(`Saved all contributions for ${year}`);
+    });
   })
   .catch((error) => console.log(error));
